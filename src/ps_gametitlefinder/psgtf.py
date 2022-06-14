d56eacb53fa032b6d5e506b04c8acd6d68f336cc
Vladimir Ventura <vladventura@outlook.es>
2022-06-14 15:24:03 -0400

    Initial commit

---
+++ src/ps_gametitlefinder/psgtf.py
@@ -0,0 +1,40 @@
+import pandas as pd
+try:
+    import importlib.resources as ir
+except ImportError:
+    import importlib_resources as ir
+from src import assets
+
+class PlaystationGame:
+    name = ""
+    id = ""
+
+    def PlaystationGame(self, name, id):
+        self.name = name
+        self.id = id
+    
+
+    def __str__(self):
+        return self.name + " " + self.id
+
+
+    def get_name(self):
+        return self.name
+
+
+    def get_id(self):
+        return self.id
+
+
+
+"""
+Gets a Playstation Game with the given ID
+"""
+def find_by_id(game_id, platform = "") -> PlaystationGame:
+    id_type = game_id[0:4]
+    id_number = game_id[4:]
+    file_loc = ir.open_text(assets, "PS3_GAMES.tsv")
+    print(file_loc)
+    df = pd.read_table(file_loc)
+    print(df)
+
